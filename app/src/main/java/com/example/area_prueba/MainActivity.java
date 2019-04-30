6ac68bb612265be330428507f5987e0816736fbf
DannyProjects <danygarcia027@gmail.com>
2019-04-30 13:08:51 -0500

    Login & SignUP Layout

--- app/src/main/java/com/example/area_prueba/MainActivity.java
+++ app/src/main/java/com/example/area_prueba/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.area_prueba;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.area_prueba.auth.RegistroActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_entrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_entrar = (Button) findViewById(R.id.btn_entrar);
+
+        btn_entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistroActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
